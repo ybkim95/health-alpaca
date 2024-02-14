284b2578c5062f28c37d7af4e523c4e2baaedda4
Yu Bin, Kim <45308022+ybkim95@users.noreply.github.com>
2024-02-14 00:35:50 -0500

    Add files via upload

---
+++ finetune.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+            
+data_path="data/finetune.json"
+output_path="medalpaca-13b"
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node=4 --master_port=2023 medalpaca/train.py \
+    --model "medalpaca/medalpaca-13b" \
+    --data_path "$data_path" \
+    --output_dir "$output_path" \
+    --train_in_8bit False \
+    --use_lora False \
+    --bf16 True \
+    --tf32 True \
+    --fp16 False \
+    --gradient_checkpointing True \
+    --global_batch_size 4 \
+    --per_device_batch_size 1 \
+    --num_epochs 5
